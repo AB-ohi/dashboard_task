290198e5e9b276f58dff6012788d7960b7c4b5b8
AB-Ohi <askohi63907@gmail.com>
2025-10-28 10:26:55 +0600

    update the home sction

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,20 @@
 import Navbar from "@/Component/Navbar/Navbar";
-import { Divide } from "lucide-react";
-import Image from "next/image";
+import Link from "next/link";
+import { ArrowRight} from "lucide-react";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Navbar />
-      <div>this is home section</div>
-    </div>
+       <div className="text-center mb-16">
+            <Link href="/dashboard/leads">
+              <button className="group bg-gradient-to-r from-blue-600 to-purple-600 text-white px-8 py-4 rounded-xl text-lg font-semibold hover:shadow-2xl transition-all duration-300 transform hover:scale-105 flex items-center gap-2 mx-auto">
+                Go to Dashboard
+                <ArrowRight className="group-hover:translate-x-1 transition-transform" size={20} />
+              </button>
+            </Link>
+          </div>
+
+    </>
   );
 }
